12e5a3445b9ffbe2dd20d349b170be494ccd74ab
Lieke22 <lieke22@gmail.com>
2014-06-25 19:08:39 +0200

    sending mail work in progress

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,25 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "Jane Doe <from@example.com>"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.mail, subject: "Random Webshop Order Confirmation"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped
+    @order = order
+
+    mail to: order.mail, subject: "Random Webshop Order Shipped"
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,18 @@ Theekoning::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer_smtp_settings = {
+      address:              "smtp.gmail.com",
+      port:                 587,
+      domain:               "domain.of.sender.net",
+      authentication:       "plain",
+      user_name:            "dave",
+      password:             "secret",
+      enable_starttls_auto: true
+
+  }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
